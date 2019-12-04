ac0e8ec917a69faa7c39aa1d1d3c66b1594ec982
Lauren Jablonski <ljablonski2@gmail.com>
2019-12-04 11:15:15 -0500

    Assign 8
    draft

---
+++ assign 8/assign8.js
@@ -0,0 +1,12 @@
+anime({
+    targets: 'div.rowBKS',
+    TranslateY: [
+        {value:2, duration: 300},
+        {value:2, duration: 0},
+    ],
+    rotate: {
+    value:'1turn',
+    /*easing: 'easeInOutSine'
+    duration: 1200,
+    loop: true*/
+}});
